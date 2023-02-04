714a7507f87ae2df2a6a9512d149dfedcc5f3a28
Aaron <115553180+aaronabele@users.noreply.github.com>
2023-02-04 17:54:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,121 @@
+//Get each arrow button
+
+const btn1 = document.querySelector(".btn-arrow1");
+const btn2 = document.querySelector(".btn-arrow2");
+const btn3 = document.querySelector(".btn-arrow3");
+const btn4 = document.querySelector(".btn-arrow4");
+const btn5 = document.querySelector(".btn-arrow5");
+
+//Declare visible variables
+
 let visible1 = false;
-let visible2 = false;
+let visible2 = true;
 let visible3 = false;
 let visible4 = false;
 let visible5 = false;
 
-const btn1 = document.querySelector("#btn-arrow1");
-document.body.style.setProperty("--none1", "none");
+//Initiate the State of the answer section
 
-const btn2 = document.querySelector("#btn-arrow2");
-document.body.style.setProperty("--none2", "");
-document.body.style.setProperty("--rotate", "180deg");
-document.body.style.setProperty("--font-style2", "800");
+function setAnswers() {
+  document.body.style.setProperty("--answer-visible1", "none");
+  document.body.style.setProperty("--answer-visible2", "");
+  document.body.style.setProperty("--answer-visible3", "none");
+  document.body.style.setProperty("--answer-visible1", "none");
+  document.body.style.setProperty("--answer-visible4", "none");
+  document.body.style.setProperty("--answer-visible5", "none");
+}
+setAnswers();
 
-const btn3 = document.querySelector("#btn-arrow3");
-document.body.style.setProperty("--none3", "none");
+//Set Fontweight
+function setFontweight() {
+  document.body.style.setProperty("--question-bold1", "400");
+  document.body.style.setProperty("--question-bold2", "800");
+  document.body.style.setProperty("--question-bold3", "400");
+  document.body.style.setProperty("--question-bold4", "400");
+  document.body.style.setProperty("--question-bol5", "400");
+}
+setFontweight();
 
-const btn4 = document.querySelector("#btn-arrow4");
-document.body.style.setProperty("--none4", "none");
+//Function to enable: Expanding and close the answer section
 
-const btn5 = document.querySelector("#btn-arrow5");
-document.body.style.setProperty("--none5", "none");
-
-function expandAndCloseFAQ() {
+function expanedAndCloseAnswer() {
   btn1.addEventListener("click", () => {
-    visible1 = !visible1;
-    if (visible1) {
-      document.body.style.setProperty("--none1", "");
-      document.body.style.setProperty("--rotate1", "180deg");
-      document.body.style.setProperty("--font-style1", "800");
-    } else {
-      document.body.style.setProperty("--none1", "none");
-      document.body.style.setProperty("--rotate1", "0deg");
-      document.body.style.setProperty("--font-style1", "400");
-    }
+    expandAndCloseAnswer1();
   });
 
   btn2.addEventListener("click", () => {
-    visible2 = !visible2;
-    if (visible2) {
-      document.body.style.setProperty("--none2", "");
-      document.body.style.setProperty("--rotate2", "180deg");
-      document.body.style.setProperty("--font-style2", "800");
-    } else {
-      document.body.style.setProperty("--none2", "none");
-      document.body.style.setProperty("--rotate2", "0deg");
-      document.body.style.setProperty("--font-style2", "400");
-    }
+    expandAndCloseAnswer2();
   });
 
   btn3.addEventListener("click", () => {
-    visible3 = !visible3;
-    if (visible3) {
-      document.body.style.setProperty("--none3", "");
-      document.body.style.setProperty("--rotate3", "180deg");
-      document.body.style.setProperty("--font-style3", "800");
-    } else {
-      document.body.style.setProperty("--none3", "none");
-      document.body.style.setProperty("--rotate3", "0deg");
-      document.body.style.setProperty("--font-style3", "400");
-    }
+    expandAndCloseAnswer3();
   });
 
   btn4.addEventListener("click", () => {
-    visible4 = !visible4;
-    if (visible4) {
-      document.body.style.setProperty("--none4", "");
-      document.body.style.setProperty("--rotate4", "180deg");
-      document.body.style.setProperty("--font-style4", "800");
-    } else {
-      document.body.style.setProperty("--none4", "none");
-      document.body.style.setProperty("--rotate4", "0deg");
-      document.body.style.setProperty("--font-style4", "400");
-    }
+    expandAndCloseAnswer4();
   });
 
   btn5.addEventListener("click", () => {
-    visible5 = !visible5;
-    if (visible5) {
-      document.body.style.setProperty("--none5", "");
-      document.body.style.setProperty("--rotate5", "180deg");
-      document.body.style.setProperty("--font-style5", "800");
-    } else {
-      document.body.style.setProperty("--none5", "none");
-      document.body.style.setProperty("--rotate5", "0deg");
-      document.body.style.setProperty("--font-style5", "400");
-    }
+    expandAndCloseAnswer5();
   });
 }
 
-expandAndCloseFAQ();
+expanedAndCloseAnswer();
+
+//Callback Functions
+
+function expandAndCloseAnswer1() {
+  visible1 = !visible1;
+  if (visible1) {
+    document.body.style.setProperty("--answer-visible1", "");
+    document.body.style.setProperty("--question-bold1", "800");
+    document.body.style;
+  } else {
+    document.body.style.setProperty("--answer-visible1", "none");
+    document.body.style.setProperty("--question-bold1", "400");
+  }
+}
+
+function expandAndCloseAnswer2() {
+  visible2 = !visible2;
+  if (!visible2) {
+    document.body.style.setProperty("--answer-visible2", "none");
+    document.body.style.setProperty("--question-bold2", "400");
+  } else {
+    document.body.style.setProperty("--answer-visible2", "");
+    document.body.style.setProperty("--question-bold2", "800");
+  }
+}
+
+function expandAndCloseAnswer3() {
+  visible3 = !visible3;
+  if (visible3) {
+    document.body.style.setProperty("--answer-visible3", "");
+    document.body.style.setProperty("--question-bold3", "800");
+  } else {
+    document.body.style.setProperty("--answer-visible3", "none");
+    document.body.style.setProperty("--question-bold3", "400");
+  }
+}
+
+function expandAndCloseAnswer4() {
+  visible4 = !visible4;
+  if (visible4) {
+    document.body.style.setProperty("--answer-visible4", "");
+    document.body.style.setProperty("--question-bold3", "800");
+  } else {
+    document.body.style.setProperty("--answer-visible4", "none");
+    document.body.style.setProperty("--question-bold3", "400");
+  }
+}
+
+function expandAndCloseAnswer5() {
+  visible5 = !visible5;
+  if (visible5) {
+    document.body.style.setProperty("--answer-visible5", "");
+    document.body.style.setProperty("--question-bold3", "800");
+  } else {
+    document.body.style.setProperty("--answer-visible5", "none");
+    document.body.style.setProperty("--question-bold3", "400");
+  }
+}
