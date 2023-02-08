4d8a50fc63f881f17cc13844b9e9b941c529e131
Aaron <115553180+aaronabele@users.noreply.github.com>
2023-02-08 13:38:41 +0100

    refactoring code

--- script.js
+++ script.js
@@ -5,36 +5,31 @@ function setFirstState() {
   for (let i = 0; i < btn.length; i++) {
     if (btn[i] != btn[1]) {
       btn[i].classList.add("invisible");
-    } else {
-      btn[i].classList.add("visible");
     }
   }
 }
-
 setFirstState();
 
 function initializeState() {
   for (let i = 0; i < btn.length; i++) {
-    if (btn[i].classList.value === "btn-arrow invisible") {
-      answer[i].style.display = "none";
-    } else {
-      answer[i].style.display = "";
+    if (btn[i].classList.value != "btn-arrow invisible") {
+      answer[i].style.display = "inline-block";
     }
   }
 }
-
 initializeState();
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", () => {
-    if (btn[i].classList.value === "btn-arrow invisible") {
-      answer[i].style.display = "";
-      btn[i].classList.remove("invisible");
-      btn[i].classList.add("visible");
-    } else if (btn[i].classList.value === "btn-arrow visible") {
-      answer[i].style.display = "none";
-      btn[i].classList.remove("visible");
-      btn[i].classList.add("invisible");
-    }
-  });
+function changeState() {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      if (btn[i].classList.value === "btn-arrow invisible") {
+        answer[i].style.display = "inline-block";
+        btn[i].classList.remove("invisible");
+      } else {
+        btn[i].classList.add("invisible");
+        answer[i].style.display = "none";
+      }
+    });
+  }
 }
+changeState();
